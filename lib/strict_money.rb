2c8cab589aa864058ba572222e6ad205106dc4f9
Francis Hwang <sera@fhwang.net>
2019-12-12 18:13:21 -0500

    Add Rubocop and Reek. (#3)

--- lib/strict_money.rb
+++ lib/strict_money.rb
@@ -1,6 +1,5 @@
 require "strict_money/version"
 
+# Manipulate money and currency with strictness around currencies and time periods.
 module StrictMoney
-  class Error < StandardError; end
-  # Your code goes here...
 end
