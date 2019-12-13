0d884feb49f562d90cdbd5563db3eea5cb5d4114
Francis Hwang <sera@fhwang.net>
2019-12-13 11:26:53 -0500

    Add StrictMoney::Amount. (#4)

--- lib/strict_money.rb
+++ lib/strict_money.rb
@@ -1,5 +1,8 @@
+require "strict_money/amount"
 require "strict_money/version"
 
 # Manipulate money and currency with strictness around currencies and time periods.
 module StrictMoney
+  # Exception raised when someone uses an invalid currency in a method call.
+  WrongCurrencyError = Class.new(StandardError)
 end
